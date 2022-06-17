7088d8f3cd2e37c71079f62d65a816481e7dc3b8
Felipe Pardo <felipepardo90@hotmail.com>
2022-06-16 21:51:15 -0300

    put products - failed

--- routes/productos.js
+++ routes/productos.js
@@ -26,7 +26,7 @@ router.post("/", (req, res) => {
       const newProduct = { ...req.body, id };
       products.push(newProduct);
       res.json("Saved");
-      return object.id;
+      return products.id;
     } catch (err) {
       console.log(`No se ha podido guardar el objeto: ${err}`);
     }
@@ -36,16 +36,22 @@ router.post("/", (req, res) => {
   res.status(500).send("Producto cargado exitosamente");
 });
 
-router.put("/:id", async (req, res) => {
- try {
-    let { id } = req.params;
-    let {title, price, thumbnail} = req.body
-    let data = await products.find(item=>item.id == id)
-    data = {title, price, thumbnail}
- } catch (error) {
-  res.json({error:"producto no encontrado"})
- }
-
+router.put("/:id",  (req, res) => {
+  const { id } = req.params;
+  const updatedProduct = products.find(item=>item.id == id)
+  console.log(updatedProduct)
+  const { title, price, thumbnail } = req.body;
+  if (title && price && thumbnail) {
+    try {
+      updatedProduct = { ...req.body};
+      products.push(updatedProduct);
+      res.json("Saved");
+      return products.id;
+    } catch (err) {
+      console.log(`No se ha podido guardar el objeto put: ${err}`);
+    }
+  } 
+  res.status(500).send("Producto actualizado exitosamente");
  
 });
 
