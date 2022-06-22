7ecd7512d77165befffe451a2258db89be01e4d6
Felipe Pardo <felipepardo90@hotmail.com>
2022-06-22 18:08:57 -0300

    usando clase en route/productos

--- src/routes/productos.js
+++ src/routes/productos.js
@@ -1,15 +1,14 @@
 const { Router } = require("express");
-const router = Router(); //Router
-const products = require('../products.json');
+const router = Router(); 
 
 // métodos
-const Contenedor = require('../class/container')
-const file = new Contenedor()
+const Contenedor = require("../class/container");
+const file = new Contenedor();
 
 // get all products
 
 router.get("/", (req, res) => {
-  let data = file.getAll()
+  let data = file.getAll();
   if (!data) {
     res.json({ error: "no hay productos" });
   }
@@ -19,10 +18,10 @@ router.get("/", (req, res) => {
 // get product by id
 
 router.get("/:id", (req, res) => {
-
-  let data = file.getById(req.params.id)
+  let data = file.getById(req.params.id);
   if (!data) {
     res.json({ error: "producto no encontrado" });
+    // res.render('404.ejs')
   }
   res.json(data);
 });
@@ -30,46 +29,32 @@ router.get("/:id", (req, res) => {
 // post
 
 router.post("/", (req, res) => {
-  const { title, price, thumbnail } = req.body;
-  if (title && price && thumbnail) {
-    try {
-      const id = products.length + 1;
-      const newProduct = { ...req.body, id };
-      products.push(newProduct);
-      res.status(500).send("Producto cargado exitosamente")
-      return products.id;
-    } catch (err) {
-      console.log(`No se ha podido guardar el objeto: ${err}`);
-    }
-  } else {
+  let data = file.save(req.body);
+  if (!data) {
     res.status(400).send("Complete los campos restantes");
   }
-  
+  res.status(200).send(`Producto ${data.id} cargado exitosamente`);
 });
 
 // put
 
 router.put("/:id", (req, res) => {
-  const { id } = req.params;
-  let data = products.findIndex((item) => item.id == id);
-  if (data != -1) {
-    req.body.id = id;
-    products[data] = req.body;
-
-    res.json(products[data]);
+  let data = file.update(req.body, req.params.id);
+  if (!data) {
+    res.status(200).json(data);
+  } else {
+    res.status(304).json({ error: "producto no encontrado" });
   }
-  res.json({ error: "producto no encontrado" });
 });
 
 // delete
 
 router.delete("/:id", (req, res) => {
-  const { id } = req.params;
-  let data = products.filter((item) => item.id != id);
+  let data = file.deleteById(req.params.id);
   if (!data) {
-    res.json({ error: "producto no encontrado" });
-  }
-  res.json(data).send("Producto eliminado");
+    res.status(304).json({ error: "producto no encontrado" });
+  } 
+  res.json(data)
 });
 
 module.exports = router;
