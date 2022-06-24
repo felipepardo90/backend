b1b7ffc9dc081d8eb678aba251ad9d026733cc26
Felipe Pardo <felipepardo90@hotmail.com>
2022-06-24 13:10:53 -0300

    controllers

---
+++ src/controllers/apiControllers.js
@@ -0,0 +1,21 @@
+
+const Contenedor = require("../class/container");
+const data = new Contenedor();
+
+
+
+const formView = (req, res)=>{
+    res.render("form")
+}
+
+const productsView = (req, res)=>{
+    let products = data.getAll()
+    res.render("products", {products})
+}
+
+const formSubmit = (req, res)=>{
+    data.save(req.body)
+    res.redirect("/")
+}
+
+module.exports={formView, productsView, formSubmit}
