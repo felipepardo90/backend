c7ae10dcf39cd0523dd3413ec4852d12c59cd9a1
Felipe Pardo <felipepardo90@hotmail.com>
2022-06-30 15:07:44 -0300

    socket on server

--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,82 @@
-const socket = io()
+const socket = io();
+
+let chatMessage = document.getElementById("chatMessage");
+let username = document.getElementById("username");
+let btn = document.getElementById("send");
+let output = document.getElementById("output");
+let actions = document.getElementById("actions");
+
+//DATE
+
+let date = new Date();
+
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+
+let hour = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
+
+// formato requerido
+let now = `${day}/${month}/${year}, ${hour}:${minutes}:${seconds}`;
+console.log(now);
+
+//
+
+btn.addEventListener("click", () => {
+  socket.emit("chat:message", {
+    message: chatMessage.value,
+    username: username.value,
+  });
+});
+
+chatMessage.addEventListener("keypress", () => {
+  socket.emit("chat:typing", username.value);
+});
+
+socket.on("chat:message", (data) => {
+  actions.innerHTML = "";
+  output.innerHTML += `
+<p>
+<strong style="color:blue">${data.username}</strong>, <span style="color:brown; font-size:12px">${now}</span>: <i style="color:green">${data.message}</i>
+</p>`;
+});
+
+socket.on("chat:typing", (data) => {
+  actions.innerHTML = `<p><em>${data} está escribiendo...</em></p>`;
+});
+
+// const messageForm = document.querySelector("#messageForm");
+// const usernameInput = document.querySelector("#usernameInput");
+// const messageInput = document.querySelector("#messageInput");
+// const messagesPool = document.querySelector("#messagesPool");
+
+// function sendMessage(messageInfo) {
+//   socket.emit("client:message", messageInfo);
+// }
+
+// function renderMessages(messagesInfo) {
+//   const html = messagesInfo
+//     .map((elem) => {
+//       return `<div>
+//         <strong>${elem.username}</strong>:
+//         <em>${elem.message}</em>
+//         </div>`;
+//     })
+//     .join(" ");
+
+//   messagesPool.innerHTML = html;
+// }
+
+// function submitHandler(event) {
+//   event.preventDefault();
+//   const messageInfo = {
+//     username: usernameInput.value,
+//     message: messageInput.value,
+//   };
+//   sendMessage(messageInfo)
+// }
+// messageForm.addEventListener("submit", submitHandler);
+
+// socket.on("server:mensajes", renderMessages);
--- src/server.js
+++ src/server.js
@@ -2,16 +2,27 @@ const app = require("./app");
 
 // starting server
 
-const server = app.listen(app.get("port"));
+const expressServer = app.listen(app.get("port"));
 console.log(
   `Servidor HTTP conectado, escuchando en el puerto ${app.get("port")}`
 );
 
-const SocketIO = require("socket.io");
-const io = SocketIO(server);
-
 // websockets
 
+const { Server: IOServer } = require("socket.io");
+const io = new IOServer(expressServer);
+
 io.on("connection", (socket) => {
-  console.log("User connected");
+  console.log(`User ${socket.id} connected`);
+  // socket.emit("server:mensajes", messagesArray)
+
+  socket.on("chat:message", (data) => {
+    io.sockets.emit("chat:message", data);
+    // messagesArray.push(messageInfo);
+    // io.emit("server:mensajes", messagesArray);
+  });
+
+  socket.on("chat:typing", (data) => {
+    socket.broadcast.emit("chat:typing", data);
+  });
 });
