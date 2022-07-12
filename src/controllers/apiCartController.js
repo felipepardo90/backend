abc66f749e2794046e37a88deba093140de6ec67
Felipe Pardo <fpardo@UTESALTA.NET>
2022-07-12 15:57:50 -0300

    Cart controllers

--- src/controllers/apiCartController.js
+++ src/controllers/apiCartController.js
@@ -1,15 +1,20 @@
 const Cart = require("../utils/Cart");
 const Producto = require("../utils/Container");
 
-
-const getAllProductsInCart = (req, res) =>{
-    const products = Producto.getAll()
-    res.json('products', {products})
-}
-
+const getAllProductsInCart = (req, res) => {
+  const products = Producto.getAll();
+  res.json("products", { products });
+};
 
 const newCart = (req, res) => {
-res.json(new Cart().getCart())
-}
+  let producto = Producto.save(req.body);
+  if (!producto) {
+    res.json({ message: "El carrito está vacío" });
+  }
+  res.render("cart", {
+    cart: Cart.save(producto),
+    pageTitle: "Nuevo producto",
+  });
+};
 
-module.exports = {newCart, getAllProductsInCart}
+module.exports = { newCart, getAllProductsInCart };
