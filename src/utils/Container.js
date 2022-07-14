0be33610d9e937ab0c9d5c9d8a90651fc8e5131d
Felipe Pardo <fpardo@UTESALTA.NET>
2022-07-14 15:58:16 -0300

    container method - pruebas

--- src/utils/Container.js
+++ src/utils/Container.js
@@ -1,101 +1,115 @@
-const fs = require('fs');
+const fs = require("fs");
 
 class Contenedor {
-    constructor(file) {
-        this.file = file;
+  constructor(file) {
+    this.file = file;
+  }
+
+  async save(object) {
+    // Recibe un objeto, lo guarda en el archivo, devuelve el id asignado.
+    try {
+      let products = JSON.parse(await fs.promises.readFile(this.file, "utf-8"));
+
+      // Archivo existente
+      !products.length
+        ? (object.id = 1)
+        : (object.id = products[products.length - 1].id + 1);
+      products.push(object);
+
+      await fs.promises.writeFile(
+        this.file,
+        JSON.stringify(products, null, "\t")
+      );
+      return object.id;
+    } catch (err) {
+      // Si el archivo no existe, lo crea
+      if (err.code === "ENOENT") {
+        object.id = 1;
+        await fs.promises.writeFile(
+          this.file,
+          JSON.stringify([object], null, "\t")
+        );
+        return object.id;
+      } else {
+        console.log("Error en método save: ", err);
+      }
     }
+  }
 
-    async save(object) {
-        // Recibe un objeto, lo guarda en el archivo, devuelve el id asignado.
-        try {
-            let products = JSON.parse(await fs.promises.readFile(this.file, 'utf-8'));
-
-            // Archivo existente
-            !products.length ? object.id = 1 : object.id = products[products.length - 1].id + 1;
-            products.push(object);
-
-            await fs.promises.writeFile(this.file, JSON.stringify(products, null, '\t'));
-            return object.id;
-        } catch (err) {
-            // Si el archivo no existe, lo crea
-            if (err.code === 'ENOENT') {
-                object.id = 1;
-                await fs.promises.writeFile(this.file, JSON.stringify([object], null, '\t'));
-                return object.id;
-            } else {
-                console.log('Error en método save: ', err);
-            }
-        }
+  async getById(number) {
+    // Recibe un id y devuelve el objeto con ese id, o null si no está.
+    try {
+      let products = JSON.parse(await fs.promises.readFile(this.file, "utf-8"));
+      const object = products.find((object) => object.id === number);
+      return object ? object : null;
+    } catch (err) {
+      console.log("Error en método getById: ", err);
     }
+  }
 
-    async getById(number) {
-        // Recibe un id y devuelve el objeto con ese id, o null si no está.
-        try {
-            let products = JSON.parse(await fs.promises.readFile(this.file, 'utf-8'));
-            const object = products.find(object => object.id === number);
-            return object ? object : null;
-        } catch (err) {
-            console.log('Error en método getById: ', err);
-        }
+  async getAll() {
+    // Devuelve un array con los objetos presentes en el archivo.
+    try {
+        return JSON.parse(await fs.promises.readFile(this.file, "utf-8"));
+    } catch (err) {
+      if (err.code === "ENOENT") {
+        return {};
+      } else {
+        console.log("Error en método getAll: ", err);
+      }
     }
+  }
 
-    async getAll() {
-        // Devuelve un array con los objetos presentes en el archivo.
-        try {
-            return JSON.parse(await fs.promises.readFile(this.file, 'utf-8'));
-        } catch (err) {
-            if (err.code === 'ENOENT') {
-                return {};
-            } else {
-                console.log('Error en método getAll: ', err);
-            }
-        }
+  async deleteById(number) {
+    // Elimina del archivo el objeto con el id buscado.
+    try {
+      let products = JSON.parse(await fs.promises.readFile(this.file, "utf-8"));
+      let productsAct = products.filter((object) => object.id != number);
+      await fs.promises.writeFile(
+        this.file,
+        JSON.stringify(productsAct, null, "\t")
+      );
+    } catch (err) {
+      console.log("Error en método deleteById: ", err);
     }
+  }
 
-    async deleteById(number) {
-        // Elimina del archivo el objeto con el id buscado.
-        try {
-            let products = JSON.parse(await fs.promises.readFile(this.file, 'utf-8'));
-            let productsAct = products.filter(object => object.id != number);
-            await fs.promises.writeFile(this.file, JSON.stringify(productsAct, null, '\t'));
-        } catch (err) {
-            console.log('Error en método deleteById: ', err);
-        }
+  async deleteAll() {
+    // Elimina todos los objetos presentes en el archivo.
+    try {
+      await fs.promises.writeFile(this.file, JSON.stringify([], null, "\t"));
+    } catch (err) {
+      console.log("Error en método deleteAll: ", err);
     }
+  }
 
-    async deleteAll() {
-        // Elimina todos los objetos presentes en el archivo.
-        try {
-            await fs.promises.writeFile(this.file, JSON.stringify([], null, '\t'));
-        } catch (err) {
-            console.log('Error en método deleteAll: ', err);
-        }
+  async updateById(id, object) {
+    // Actualiza un objeto según su id. DEvuelve el objeto, o no encontrado
+    try {
+      let products = JSON.parse(await fs.promises.readFile(this.file, "utf-8"));
+      object.id = id;
+
+      const index = products.findIndex((product) => {
+        return product.id === object.id;
+      });
+
+      if (index !== -1) {
+        products[index] = object;
+        await fs.promises.writeFile(
+          this.file,
+          JSON.stringify(products, null, "\t")
+        );
+        return object;
+      } else {
+        return { error: "Producto no encontrado" };
+      }
+    } catch (err) {
+      console.log("Error en método updateById: ", err);
     }
-
-    async updateById(id, object) {
-        // Actualiza un objeto según su id. DEvuelve el objeto, o no encontrado
-        try {
-            let products = JSON.parse(await fs.promises.readFile(this.file, 'utf-8'));
-            object.id = id;
-            
-            const index = products.findIndex((product) => {
-                return product.id === object.id;
-            })
-
-            if (index !== -1) {
-                products[index] = object;
-                await fs.promises.writeFile(this.file, JSON.stringify(products, null, '\t'));
-                return object;
-            } else {
-                return { error: 'Producto no encontrado'}
-            }
-        } catch (err) {
-            console.log('Error en método updateById: ', err);
-        }
-    }
+  }
 }
 
-module.exports = Contenedor
+module.exports = Contenedor;
 
 // const fs = require("fs");
 
@@ -124,7 +138,7 @@ module.exports = Contenedor
 //     // actualiza un objecto según el ID
 
 //     let updatedData = this.products.findIndex(({ id }) => id == clientId);
-  
+
 //     if (updatedData == -1) {
 //       return null;
 //     }
@@ -179,16 +193,6 @@ module.exports = Contenedor
 
 // module.exports = Contenedor;
 
-// const container = new Contenedor();
+// const container = new Contenedor("./products.json");
 
-// console.log(
-//   container.update(
-//     {
-//       title: "Submarino",
-//       price: 300,
-//       thumbnail:
-//         "https://www.recetas-argentinas.com/base/stock/Recipe/280-image/280-image_web.jpg",
-//     },
-//     3
-//   )
-// );
+// console.log(container.getAll());
